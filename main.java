6e989dc9008e20cf2d16b739f00d7ce1acc031e4
Pablo <pablo.sanchez.2505@gmail.com>
2026-04-05 00:02:00 +0200

    ultimos destalles

--- main.java
+++ main.java
@@ -13,17 +13,20 @@ public class Main{
         switch (dificultad) {
             case 1:
                 num = (int)(Math.random() * 10) + 1;
-                System.out.println("Has elegido el nivel facil"+num);
+                System.out.println("Has elegido el nivel facil");
+                System.out.println("El numero es del 1 al 10");
                 adivinar(num);
                 break;
             case 2:
                 num = (int)(Math.random() * 20) + 1;
                 System.out.println("Has elegido el nivel medio");
+                System.out.println("El numero es del 1 al 20");
                 adivinar(num);
                 break;
             case 3:
                 num = (int)(Math.random() * 30) + 1;
                 System.out.println("Has elegido el nivel dificil");
+                System.out.println("El numero es del 1 al 30");
                 adivinar(num);
                 break;
 
@@ -37,11 +40,18 @@ public class Main{
         int intentos=1;
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Di el numero q crees q es ");
+            System.out.println("Di el numero que crees que es ");
             int numero = scanner.nextInt();
             intentos = intentos+1;
+            System.out.println("Intento:"+intentos);
+            if(numero<num){
+             System.out.println("El numero es mas alto");
+            }
+            if(numero>num){
+             System.out.println("El numero es mas bajo");
+            }
             if (intentos==5){
-                System.out.println("Has perdido");
+                System.out.println("Has perdido,el numero era: "+num);
                 break;
             }
             if (numero==num) {
